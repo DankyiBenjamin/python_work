64727a9ed89584cc9e3ca7ced45ea25f8f4830db
“Dankyi <dankyibenjamin0@gmail.com>
2022-07-29 12:35:08 +0000

    commit

--- OA/studybud/settings.py
+++ OA/studybud/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ng07px_w4c(i)q=44i2y4f8d4r8a9gm9v!9fb-c845de!3d1a#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','thecounsil.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- OA/studybud/urls.py
+++ OA/studybud/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
+# from django.views.static import serve
+# from django.conf.urls import url
 
 
 
@@ -30,8 +30,8 @@ urlpatterns = [
     path('',include('base.urls')),
     
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
   
 ]
 
